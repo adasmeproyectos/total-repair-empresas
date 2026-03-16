81e5f68c1755a54c578a5033968ad6113fb9ca6e
adasmeproyectos <contacto.adasme@gmail.com>
2026-03-16 18:21:20 -0300

    Configuración multi-página de Vite añadida

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nosotros: resolve(__dirname, 'nosotros.html'),
+        servicios: resolve(__dirname, 'servicios.html'),
+        contacto: resolve(__dirname, 'contacto.html')
+      }
+    }
+  }
+});
